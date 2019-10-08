c9dbf418d8f5634d77821f7f880d411e15c115dc
威龙 <1273559213@qq.com>
2019-10-08 13:45:44 +0800

    [add]SpringBoot集成Swagger2

--- server/book_server/src/main/java/com/book/controller/TestController.java
+++ server/book_server/src/main/java/com/book/controller/TestController.java
@@ -1,16 +1,34 @@
 package com.book.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@EnableAutoConfiguration
+@RequestMapping("Test")
+@Api(tags = "测试接口")
 public class TestController {
-    @RequestMapping("/Test")
+    @GetMapping("/get")
     private String HaHa()
     {
         return "hallos";
+    }
+    @PostMapping("/add")
+    private  boolean AddTest(@RequestBody String data){
+        System.out.println(data);
+        return true;
+    }
+
+    @PutMapping("/Update")
+    private  boolean UpdateTest(@RequestBody String data){
+        System.out.println(data);
+        return true;
+    }
+
+    @DeleteMapping("/Delete/{id}")
+    private  boolean DeleteTest(@PathVariable("id") int data){
+        System.out.println(data);
+        return true;
     }
 
 }
